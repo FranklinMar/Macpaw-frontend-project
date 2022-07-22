171a137d9133fc43dd99fcbc365bf547c697ed77
denisbereznuuk <denisbereznuuk@gmail.com>
2022-07-22 17:18:21 +0300

    fix(grid): refactored grid section to more simple grid layout.
    HTML: Removed unnecessary nested 'div' nodes. Grid refactor to list of 'item' nodes. Now only 'section' elements and 'reverse' attribute is required for swapping items.
    
    CSS: CSS grid second use with layout for different proportions. Using 'reverse' attribute for reversing layout (see Figma model).
    
    JS: Generic 'Limit' functionality for multiple use.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 
-var sectionColumn1 = '<div class="column-small">\n' +
-    '                <div class="item">\n' +
-    '                  <div class="label">Hello</div>\n' +
-    '                </div>\n' +
-    '                <div class="item"></div>\n' +
-    '              </div>';
-var sectionColumn2 = '<div class="column">\n' +
-    '                <div class="row">\n' +
-    '                  <div class="item"></div>\n' +
-    '                    <div class="item"></div>\n' +
-    '                </div>\n' +
-    '                <div class="item"></div>\n' +
-    '              </div>';
+// var sectionColumn1 = '<div class="column-small">\n' +
+//     '                <div class="item">\n' +
+//     '                  <div class="label">Hello</div>\n' +
+//     '                </div>\n' +
+//     '                <div class="item"></div>\n' +
+//     '              </div>';
+// var sectionColumn2 = '<div class="column">\n' +
+//     '                <div class="row">\n' +
+//     '                  <div class="item"></div>\n' +
+//     '                    <div class="item"></div>\n' +
+//     '                </div>\n' +
+//     '                <div class="item"></div>\n' +
+//     '              </div>';
 var likeSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" fill="none" viewBox="0 0 20 24">\n' +
     '  <path fill="#FF868E" fill-rule="evenodd" d="M5.38 1.803c-2.235 0-4.047 2.451-4.047 5.475 0 1.452.427 2.844 1.186 3.87L10 21.269l7.481-10.12c.76-1.026 1.186-2.418 1.186-3.87 0-3.024-1.812-5.475-4.048-5.475-1.073 0-2.103.577-2.862 1.604l-1.286 1.74c-.26.351-.682.351-.942 0l-1.286-1.74C7.483 2.38 6.454 1.803 5.38 1.803ZM0 7.278C0 3.258 2.409 0 5.38 0c1.428 0 2.796.767 3.805 2.132L10 3.233l.815-1.101C11.824.767 13.192 0 14.619 0 17.591 0 20 3.258 20 7.278c0 1.93-.567 3.78-1.576 5.146L10.471 23.18c-.26.352-.682.352-.942 0L1.576 12.424C.566 11.059 0 9.208 0 7.278Z" clip-rule="evenodd"/>\n' +
     '</svg>\n';
@@ -79,19 +79,27 @@ for (let button of buttons) {
     button.addEventListener('click', buttonsListener);
 }
 
-function limitSelect() {
-    let select = document.getElementById("limit");
-    let value = select.options[select.selectedIndex].value;
+function limitSelect(thiss) {
+    // let select = document.getElementById(id);
+    let value = thiss.options[thiss.selectedIndex].value;
     // value = value.toString().replace("Limit: ", "");
     let grid = document.getElementsByClassName("grid")[0];
     grid.innerHTML = "";
     // grid.innerHTML = sectionContent.repeat(value/5);
-    for (let i = 0; i < value / 5; i++) {
+    console.log(value);
+    for (let i = 0; i < value /*/ 5*/; i++) {
         let section = document.createElement('section');
         if (i%2) {
-            section.innerHTML += sectionColumn2 + sectionColumn1;
-        } else {
-            section.innerHTML += sectionColumn1 + sectionColumn2;
+            section.setAttribute("reverse", "");
+            // section.innerHTML += sectionColumn2 + sectionColumn1;
+        // } else {
+            // section.innerHTML += sectionColumn1 + sectionColumn2;
+        }
+        for (let j = 0; j < 5; j++) {
+            section.innerHTML += '<div class="item">\n' +
+                '<div class="label">Hello</div>\n' +
+                '</div>\n';
+            // let item = document.createElement('div');
         }
         grid.append(section);
     }
