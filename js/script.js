3073329105a0478016cdf3d58d969b6079fa283d
denisbereznuuk <denisbereznuuk@gmail.com>
2022-07-25 02:49:34 +0300

    feat(Gallery-CatAPI): Added Type and Breed selection, refactored generation of content inside grid and completed design.
    HTML: Added refresh button
    
    CSS: Styles for refresh button and refactored grid items placement.
    
    JS: Refactor of limitListener to itemGenerator for changing amount of items in grid. Added Type and Breed to the query. Added dynamic select options for breed. Optimized functions and global variables.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var likeSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" fill="none" viewBox="0 0 20 24">\n' +
+const likeSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" fill="none" viewBox="0 0 20 24">\n' +
     '\t<path fill="#FF868E" fill-rule="evenodd" d="M5.38 1.803c-2.235 0-4.047 2.451-4.047 5.475 0 1.452.427 2.844 ' +
     '1.186 3.87L10 21.269l7.481-10.12c.76-1.026 1.186-2.418 1.186-3.87 0-3.024-1.812-5.475-4.048-5.475-1.073 ' +
     '0-2.103.577-2.862 1.604l-1.286 1.74c-.26.351-.682.351-.942 0l-1.286-1.74C7.483 2.38 6.454 1.803 5.38 1.803ZM0 ' +
@@ -7,30 +7,20 @@ var likeSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" fi
     '11.059 0 9.208 0 7.278Z" clip-rule="evenodd"/>\n' +
     '</svg>\n';
 
-/*String.prototype.format = function () {
-    // store arguments in an array
-    var args = arguments;
-    // use replace to iterate over the string
-    // select the match and check if related argument is present
-    // if yes, replace the match with the argument
-    return this.replace(/{(\d+)}/g, function (match, index) {
-        // check if the argument is present
-        return typeof args[index] == 'undefined' ? match : args[index];
-    });
-};*/
-
-
 let buttons = document.getElementById("menu").getElementsByTagName("input");
 let check = null;
 let home = document.getElementById("home");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+let mainContent = document.getElementById("main-content");
 
-let key = "af5697f4-7d20-4967-9fad-94df7846fcd3";
-let breedsListUrl = "https://api.thecatapi.com/v1/breeds?attach_breed=0";
+const key = "af5697f4-7d20-4967-9fad-94df7846fcd3";
+const breedsListUrl = "https://api.thecatapi.com/v1/breeds?attach_breed=0";
 // let gallerySearchUrl = "https://api.thecatapi.com/v1/images/search?size=full&limit={0}&page={1}&order=ASC";
 let gallerySearchUrl = "https://api.thecatapi.com/v1/images/search?size=thumb";
 
-let page = 0, listLength = 0, limit = 0;
-let list = null, order = "RANDOM";
+let page = 0, listLength = 0, limit = 5;
+let list = null, order = "RANDOM", breed = null, type = null;
 
 
 // Buttons check listener
@@ -51,12 +41,8 @@ function buttonsListener() {
         for (let i of document.getElementsByClassName(this.id.toString() + "-selector")) {
             i.style.removeProperty("display");
         }
-        // if (document.getElementById('breeds-check').checked) {
-        if (this.id === 'breeds') {
-            // resetGrid();
-            // let content = document.getElementById("main-content");
-            // content.innerHTML = "";
 
+        if (this.id === 'breeds') {
             title = "BREEDS";
             let items = document.getElementsByClassName("item");
             for (let i of items) {
@@ -65,17 +51,31 @@ function buttonsListener() {
                 i.firstElementChild.innerHTML = "Breed";
             }
         } else if (this.id === 'gallery') {
-            if (order === "RANDOM") {
-                document.getElementById("next").setAttribute("disabled", "");
-            } else {
-                document.getElementById("next").removeAttribute("disabled");
-            }
-
-            // resetGrid();
             title = "GALLERY";
+            if (order === "RANDOM") {
+                next.setAttribute("disabled", "");
+            } else {
+                next.removeAttribute("disabled");
+            }
+            let breeds = document.getElementById("galleryBreeds");
+            let breedsList = ajax_get(breedsListUrl).data;
+            let option;
+            for (let item of breedsList) {
+                option = document.createElement("option");
+                option.value = item.id;
+                option.innerText = item.name;
+                breeds.appendChild(option);
+            }
             // galleryLimit(document.getElementById("galleryLimit"));
 
-            limitListener(limit < 5 ? 5 : limit, page);
+
+            if (page === listLength/limit) {
+                next.setAttribute("disabled", "");
+            } else if(next.hasAttribute("disabled")) {
+                next.removeAttribute("disabled");
+            }
+            itemGenerator(limit/*limit < 5 ? 5 : limit*/);
+            changePage(limit, page, order, breed, type);
             let items = document.getElementsByClassName("item");
             for (let i of items) {
                 i.style.alignItems = "center";
@@ -101,50 +101,58 @@ function buttonsListener() {
 
 function breedsLimit(element){
     page = 0;
-    document.getElementById("prev").setAttribute("disabled", "");
+    prev.setAttribute("disabled", "");
     // limit = element.options[element.selectedIndex].value;
     // changePage(limit, page);
-    limitListener(element.options[element.selectedIndex].value, page);
+    itemGenerator(element.options[element.selectedIndex].value);
     let items = document.getElementsByClassName("item");
     for (let i of items) {
         i.style.alignItems = "end";
         i.firstElementChild.style.width = "100%";
         i.firstElementChild.innerHTML = "Breed";
     }
+    changePage(limit, page, order, breed, type);
 }
 
 function galleryLimit(element){
     page = 0;
-    document.getElementById("prev").setAttribute("disabled", "");
+    prev.setAttribute("disabled", "");
     // limit = element.options[element.selectedIndex].value;
     // changePage(limit, page);
-    limitListener(element.options[element.selectedIndex].value, page);
+    itemGenerator(element.options[element.selectedIndex].value);
     let items = document.getElementsByClassName("item");
     for (let i of items) {
         i.style.alignItems = "center";
         i.firstElementChild.style.width = "fit-content";
         i.firstElementChild.innerHTML = likeSvg;
     }
+    changePage(limit, page, order, breed, type);
 }
 
-function limitListener(curLimit, curPage) {
-    if (curLimit !== limit) {
-        let content = document.getElementById("main-content");
+function itemGenerator(curLimit) {
+    let items = mainContent.getElementsByClassName("item").length;
+
+
+    // console.log(`CURLIMIT: ${curLimit} ; ITEMLENGTH: ${items}`);
+
+
+    if (curLimit !== items) {
         // content.innerHTML = "";
         let section, item, label;
         // let iterator = list.entries();
-        if (Number(curLimit) > Number(limit)) {
-            for (let i = 0; i < curLimit - limit; i++) {
+        if (Number(curLimit) > Number(items)) {
+            for (let i = 0; i < curLimit - items; i++) {
                 if (i%5 === 0) {
                     section = document.createElement("section");
                     // if (Math.round(i/5) % 2) {
                     //     section.setAttribute("reverse", "");
                     // }
-                    content.append(section);
+                    mainContent.append(section);
                 }
 
                 item = document.createElement("div");
                 item.setAttribute("class", "item");
+                item.style.setProperty("--background", "none");
                 // item.setAttribute("style", "--background: url( );")
                 // item.style = `--background: url(${list[i].url});`;
                 // item.style = `--background: ${iterator.next().value.url};`;
@@ -154,24 +162,43 @@ function limitListener(curLimit, curPage) {
                 section.appendChild(item);
             }
         } else {
-            let section = content.getElementsByTagName("section");
+            let items = mainContent.getElementsByClassName("item");
             //console.log((limit - curLimit)/5);
-            for (let i = (limit - curLimit)/5; i >= curLimit/5; i--) {
-                content.removeChild(section[i]);
+            let parent;
+
+
+            // console.log(`INSIDE GENERATOR\nITEMS: ${items.length}\nCURLIMIT: ${curLimit}`)
+
+
+            for (let i = items.length - 1; i >= curLimit; i--) {
+                parent = items[i].parentNode;
+                items[i].remove();
+
+
+                // console.log(`INSIDE GENERATOR\nPARENT CHILD ITEMS: ${parent.childElementCount}`);
+
+
+                if(parent.childElementCount === 0) {
+                    parent.parentElement.removeChild(parent);
+                }
             }
+
+            /*let section = mainContent.getElementsByTagName("section");
+            for (let i = (items - curLimit)/5; i >= curLimit/5; i--) {
+                mainContent.removeChild(section[i]);
+            }*/
         }
         limit = curLimit;
     }
-    changePage(curLimit, curPage, order);
 }
 
 function prevPage(element) {
     //console.log(page);
     if (page > 0) {
         page--;
-        changePage(limit, page, order);
+        changePage(limit, page, order, breed, type);
 
-        document.getElementById("next").removeAttribute("disabled");
+        next.removeAttribute("disabled");
     }
     if (page === 0) {
         element.setAttribute("disabled", "");
@@ -185,8 +212,8 @@ function nextPage(element) {
     if (page < listLength/limit) {
         page++;
 
-        document.getElementById("prev").removeAttribute("disabled");
-        changePage(limit, page, order);
+        prev.removeAttribute("disabled");
+        changePage(limit, page, order, breed, type);
     }
 
     if (page === listLength/limit) {
@@ -198,19 +225,39 @@ function nextPage(element) {
 
 function orderListener(element) {
     page = 0;
-    document.getElementById("prev").setAttribute("disabled", "");
+    prev.setAttribute("disabled", "");
     order = element.options[element.selectedIndex].value;
     if (order === "RANDOM") {
-        document.getElementById("next").setAttribute("disabled", "");
+        next.setAttribute("disabled", "");
         //console.log(true);
     } else {
-        document.getElementById("next").removeAttribute("disabled");
+        next.removeAttribute("disabled");
         //console.log(false);
     }
-    changePage(limit, page, order);
+    changePage(limit, page, order, breed, type);
 }
 
-function changePage(curLimit, curPage, curOrder) {
+function galleryBreedsListener(element) {
+    page = 0;
+    prev.setAttribute("disabled", "");
+    breed = element.options[element.selectedIndex].value;
+    if (breed === "none") {
+        breed = null;
+    }
+    changePage(limit, page, order, breed, type);
+}
+
+function typeListener(element) {
+    page = 0;
+    prev.setAttribute("disabled", "");
+    type = element.options[element.selectedIndex].value;
+    if (type === "none") {
+        type = null;
+    }
+    changePage(limit, page, order, breed, type);
+}
+
+function changePage(curLimit, curPage, curOrder, curBreed, curType) {
     let query = gallerySearchUrl;
     if (curLimit != null) {
         query += `&limit=${curLimit}`;
@@ -223,42 +270,86 @@ function changePage(curLimit, curPage, curOrder) {
     if (curOrder != null) {
         query += `&order=${curOrder}`;
     }
+    if (curBreed != null) {
+        query += `&breed_id=${curBreed}`;
+    }
+    if (curType != null) {
+        query += `&mime_types=${curType}`;
+    }
     let obj = ajax_get(query);
     list = obj.data;
     listLength = obj.length;
 
-    let content = document.getElementById("main-content");
-    let items = content.getElementsByClassName("item");
+    // let items = mainContent.getElementsByClassName("item");
+    // console.log("WHY ISN'T IT WORKING?");
+
+
+    // console.log(`LIMIT: ${limit} ; LENGTH: ${list.length}`);
+
+
+    if (limit > (Math.ceil(list.length / 5) * 5)) {
+        limit = (Math.ceil(list.length / 5) * 5);
+        // limit -= 5;
+        // itemGenerator(limit - Math.ceil(list.length / 5) * 5);
+        itemGenerator(list.length);
+        // items = mainContent.getElementsByClassName("item");
+    }
+    let items = mainContent.getElementsByClassName("item");
     let i = 0;
+
+
+    // console.log(`ITEMS: ${items.length} ; LENGTH: ${list.length}`);
+
+
+    // console.log("WORKED!");
     for (let item of items) {
         // item.style.toString().replace("/\\--background: url(.*?\\);/g", `--background: url(${list[i].url});`);
         // item.style.backgroundImage = `url(${list[i].url})`;
-        item.style.setProperty('--background', `url(${list[i].url}`);
+        /*if (list[i]) {
+            console.log(list[i]);
+            item.style.setProperty('--background', `url(${list[i].url})`);
+        } else {
+            item.remove();
+        }*/
+        // if (i > list.length - 1) {
+        //     item.remove();
+        // } else {
+            item.style.setProperty('--background', `url(${list[i].url})`);
+        // }
         i++;
     }
+
+
+    // items = mainContent.getElementsByClassName("item");
+    // console.log(`ITEMS: ${items.length} ; LENGTH: ${list.length}`);
+
+
+    /*let sections = mainContent.getElementsByTagName("section");
+    for (let section of sections) {
+        if (section.getElementsByClassName('item').length === 0) {
+            section.remove();
+        }
+    }*/
 }
 
 function ajax_get(url) {
     console.log(url);
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.addEventListener("readystatechange", function () {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             console.log(this.responseText);
         }
     });
-    xmlhttp.open("GET", url, false);
-    xmlhttp.setRequestHeader("x-api-key", key);
-    // xmlhttp.setRequestHeader("Access-Control-Expose-Headers", "Pagination-Count");
-    xmlhttp.send();
-    console.log(xmlhttp.getAllResponseHeaders());//getResponseHeader("Pagination-Count"));
-    return {"data": JSON.parse(xmlhttp.responseText), "length": order !== "RANDOM" ?
-            xmlhttp.getResponseHeader("Pagination-Count") : 0};
+    xmlHttp.open("GET", url, false);
+    xmlHttp.setRequestHeader("x-api-key", key);
+    // xmlHttp.setRequestHeader("Access-Control-Expose-Headers", "Pagination-Count");
+    xmlHttp.send();
+    // console.log(xmlHttp.getAllResponseHeaders());//getResponseHeader("Pagination-Count"));
+    return {"data": JSON.parse(xmlHttp.responseText), "length": order !== "RANDOM" ?
+            xmlHttp.getResponseHeader("Pagination-Count") : 0};
 }
 
 // Adding buttons listeners
 for (let button of buttons) {
     button.addEventListener('click', buttonsListener);
 }
-
-// Initializing Grid page block
-//resetGrid();
