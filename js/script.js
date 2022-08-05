a71af8bec2d882b66cb30a9c420f51ce3ef1568f
denisbereznuuk <denisbereznuuk@gmail.com>
2022-08-05 22:23:06 +0300

    feat(JavaScript-Votes): Added "votesHandler" and "votesListener" for Votes section.
    JS: Partially functional "Votes" part of the page. "votesHandler" - finds random image for voting and prints logs of actions committed (only Like or Dislike). "votesListener" - event handler of button click (Like, Add to favourites or Dislike) and builds up HTTP request for the action.

--- js/script.js
+++ js/script.js
@@ -5,30 +5,50 @@ const likeSvg = '<svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0
     '6.071 0 0 0 8.071 2ZM0 8.071a8.071 8.071 0 0 1 13.778-5.707L15 3.586l1.222-1.222a8.071 8.071 0 0 1 11.414 ' +
     '11.414l-11.929 11.93a1 1 0 0 1-1.414 0L2.364 13.777A8.071 8.071 0 0 1 0 8.071Z" clip-rule="evenodd"/>\n</svg>';
 
+const smileSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none" viewBox="0 0 30 30">\n' +
+    '<path fill="#97EAB9" fill-rule="evenodd" d="M0 15C0 6.716 6.716 0 15 0c8.284 0 15 6.716 15 15 0 8.284-6.716 ' +
+    '15-15 15-8.284 0-15-6.716-15-15ZM15 2C7.82 2 2 7.82 2 15s5.82 13 13 13 13-5.82 13-13S22.18 2 15 2Zm-5 ' +
+    '10H8v-2h2v2Zm12 0h-2v-2h2v2ZM9.2 16.6l.6.8c2.6 3.467 7.8 3.467 10.4 0l.6-.8 1.6 1.2-.6.8c-3.4 4.533-10.2 ' +
+    '4.533-13.6 0l-.6-.8 1.6-1.2Z" clip-rule="evenodd"/>\n</svg>';
+
+const sadSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none" viewBox="0 0 30 30">\n' +
+    '<path fill="#FFD280" fill-rule="evenodd" d="M0 15C0 6.716 6.716 0 15 0c8.284 0 15 6.716 15 15 0 8.284-6.716 ' +
+    '15-15 15-8.284 0-15-6.716-15-15ZM15 2C7.82 2 2 7.82 2 15s5.82 13 13 13 13-5.82 13-13S22.18 2 15 2Zm-5 ' +
+    '10H8v-2h2v2Zm12 0h-2v-2h2v2ZM7.6 20.2l.6-.8c3.4-4.533 10.2-4.533 13.6 ' +
+    '0l.6.8-1.6 1.2-.6-.8c-2.6-3.467-7.8-3.467-10.4 0l-.6.8-1.6-1.2Z" clip-rule="evenodd"/>\n</svg>';
+
 let buttons = document.getElementById("menu").getElementsByTagName("input");
-let check = null;
+let check = null, checkBreedId = null;
 // let home = document.getElementById("home");
+// let options = document.getElementById("options");
+let titleNode = document.getElementById("title");
+let content = document.getElementById('content');
+
+let mainContent = document.getElementById("main-content");
 let prev = document.getElementById("prev");
 let next = document.getElementById("next");
-// let options = document.getElementById("options");
-let content = document.getElementById('content');
-let mainContent = document.getElementById("main-content");
+
 let galleryBreeds = document.getElementById("galleryBreeds");
 let breedsBreeds = document.getElementById("breedsBreeds");
+
+let voteImg = document.getElementById("vote-img");
+let logs = document.getElementById("logs");
+let favVote = document.getElementById("fav-vote")
 
 const key = "af5697f4-7d20-4967-9fad-94df7846fcd3";
 const sub = "user-123";
 // const breedsListUrl = "https://api.thecatapi.com/v1/breeds?attach_breed=0";
 const breedsListUrl = "https://api.thecatapi.com/v1/breeds?";
 // let gallerySearchUrl = "https://api.thecatapi.com/v1/images/search?size=full&limit={0}&page={1}&order=ASC";
-let gallerySearchUrl = "https://api.thecatapi.com/v1/images/search?size=thumb";
+let gallerySearchUrl = "https://api.thecatapi.com/v1/images/search";
 let favUrl = "https://api.thecatapi.com/v1/favourites";
+let votesUrl = "https://api.thecatapi.com/v1/votes";
 
 
 let favourites;
 
 (async function(){
-    favourites = await ajax_get(favUrl + `?sub_id=${sub}`, "GET");
+    favourites = await ajax(favUrl + `?sub_id=${sub}`, "GET");
     favourites = favourites.data;
 })();
 
@@ -47,13 +67,13 @@ class QueryParams {
 
 
 let gallery = new QueryParams({"limit": 5, "page": 0, "order": null, "breed_id": null, "mime_types": null},
-    gallerySearchUrl);
+    gallerySearchUrl + "?size=thumb");
 gallery.change = (function () {
     let items = document.getElementsByClassName("item");
 
     let index = 0;
     let fav;
-    // favourites = await ajax_get(favUrl + `?sub_id=${sub}`, "GET");
+    // favourites = await ajax(favUrl + `?sub_id=${sub}`, "GET");
     // favourites = favourites.data;
     // console.log(favourites);
     for (let i of items) {
@@ -91,6 +111,8 @@ breeds.check = document.querySelector('input[name="sort"]:checked');//null; CHEC
 });
 let currentParam;
 
+let voteObject;
+
 
 
 // Buttons check listener
@@ -117,16 +139,22 @@ function buttonsListener() {
             title = "BREEDS";
             currentParam = breeds;
 
-            void changePage(breeds, breeds.list.length === 0);
+            if (checkBreedId == null) {
+                titleNode.classList.remove("fade");
+                void changePage(breeds, breeds.list.length === 0);
+            } else {
+                titleNode.classList.add("fade");
+            }
         } else if (this.id === 'gallery') {
             title = "GALLERY";
             currentParam = gallery;
             void changePage(gallery, gallery.list.length === 0);
         } else if (this.id === 'voting') {
             title = "VOTING";
+            void votesHandler();
         }
         // currentParam.changeItems();
-        document.getElementById("title").innerText = title;
+        titleNode.innerText = title;
     } else {
 
         for (let button of buttons) {
@@ -139,6 +167,70 @@ function buttonsListener() {
         content.style.display = "none";
         $("#home").fadeIn();
     }
+}
+
+async function voteListener(value) {
+    let body = {
+        "image_id": voteObject.id,
+        "sub_id": sub
+    };
+    let url, method;
+    let fav = favourites.find(obj => obj['image_id'] === voteObject.id);
+    if (Number(value) >= 0) {
+        body["value"] = value;
+        url = votesUrl;
+        method = "POST";
+    } else {
+        url = fav ? favUrl + `/${fav.id}` : favUrl;
+        method = fav ? "DELETE" : "POST";
+        body = fav ? undefined : body;
+        if (fav) {
+            favVote.classList.remove("fav");
+        } else {
+            favVote.classList.add("fav");
+        }
+    }
+    await ajax(url, method, body);
+    if (Number(value) >= 0) {
+        await votesHandler();
+    } else {
+        favourites = await ajax(favUrl + `?sub_id=${sub}`, "GET");
+        favourites = favourites.data;
+    }
+}
+
+async function votesHandler() {
+    /*if (!voteObject) {
+    /!*let image*!/voteObject = await ajax(gallerySearchUrl + `?size=full`, "GET");
+    /!*image = image.data[0];*!/
+        voteObject = voteObject.data[0];
+    }*/
+    favVote.classList.remove("fav");
+    voteObject = await ajax(gallerySearchUrl + `?size=full`, "GET");
+    voteObject = voteObject.data[0];
+    let votes = await ajax(votesUrl + `?order=DESC&sub_id=${sub}`, "GET");
+    // console.log(votes.data);
+    // votes = votes.data;
+    logs.innerHTML = "";
+    let date, log, strong, p;
+    for (let i of votes.data) {
+        log = document.createElement("div");
+        log.classList.add("log");
+        strong = document.createElement("strong");
+        date = new Date(i['created_at']);
+        strong.innerText = `${date.getHours()}:${date.getMinutes()}`;
+        log.appendChild(strong);
+        p = document.createElement("p");
+        p.innerHTML = `Image ID: <span>${i['image_id']}</span> was added to ${ i.value > 0 ? "Likes" : "Dislikes"}`;
+        // console.log(date);
+        log.appendChild(p);
+        log.innerHTML = log.innerHTML + ((i.value > 0) ? smileSvg : sadSvg);
+        logs.appendChild(log);
+    }
+    // console.log(votes[0]);
+    // console.log(image);
+    // console.log(image.url);
+    voteImg.src = voteObject.url;/*image.url;*/
 }
 
 function limitBreeds(element){
@@ -189,7 +281,7 @@ async function breedsListener(element) {
     if (breed !== "0") {
         breeds.list = [Object.assign({}, breeds.fullList.find(i => i.id === breed))];
         // console.log()
-        breeds.list[0].image = (await ajax_get(`https://api.thecatapi.com/v1/images/search?size=thumb&order=ASC&breed_id=${
+        breeds.list[0].image = (await ajax(`https://api.thecatapi.com/v1/images/search?size=thumb&order=ASC&breed_id=${
             breed}`, "GET")).data[0];
     } else {
         breeds.list = breeds.fullList.slice(breeds.dict.page * breeds.dict.limit,
@@ -265,7 +357,7 @@ async function changePage(params, queryBool=true) {
                 query += `&${i}=${params.dict[i]}`;
             }
         }
-        let obj = await ajax_get(query, "GET");
+        let obj = await ajax(query, "GET");
         params.list = obj.data;
         params.listLength = obj.length == null ? params.list.length : obj.length;
     }
@@ -338,25 +430,26 @@ function itemGenerator(curLimit) {
 }
 
 async function clickListener() {
+    let id = this.getAttribute("data-id");
     if (check.id === 'breeds') {
-
+        checkBreedId = id;
     } else if (check.id === 'gallery') {
         if (!this.classList.contains("fav")) {
             let body = {
-                "image_id": this.getAttribute("data-id"),
+                "image_id": id,
                 "sub_id": sub
             };
-            let fav = await ajax_get(favUrl, "POST", body);
+            let fav = await ajax(favUrl, "POST", body);
             fav = fav.data;
-            // void ajax_get(favUrl + `?sub_id=${sub}`, "GET");
+            // void ajax(favUrl + `?sub_id=${sub}`, "GET");
             this.classList.add("fav");
             this.setAttribute("data-fav-id", fav.id);
         } else {
-            // void ajax_get(favUrl + `/${this.getAttribute("data-id")}`, "DELETE");
-            void await ajax_get(favUrl + `/${this.getAttribute("data-fav-id")}`, "DELETE");
+            // void ajax(favUrl + `/${this.getAttribute("data-id")}`, "DELETE");
+            void await ajax(favUrl + `/${this.getAttribute("data-fav-id")}`, "DELETE");
             this.classList.remove("fav");
         }
-        favourites = await ajax_get(favUrl + `?sub_id=${sub}`, "GET");
+        favourites = await ajax(favUrl + `?sub_id=${sub}`, "GET");
         favourites = favourites.data;
     }
 }
@@ -377,7 +470,7 @@ function checking(params) {
     return bool;
 }
 
-async function ajax_get(url, method, body) {
+async function ajax(url, method, body) {
     console.log(url);
     let object = {
         method: method,
@@ -465,14 +558,14 @@ for (let button of buttons) {
 }
 
 // Initializing breeds list
-// let breedsObj = ajax_get(breedsListUrl);
+// let breedsObj = ajax(breedsListUrl);
 (async function(){
     let option;
-    // let obj = await ajax_get(breedsListUrl, "GET");
+    // let obj = await ajax(breedsListUrl, "GET");
     // console.log(obj);
     // console.log(obj.data);
     // breeds.fullList = obj.data;
-    breeds.fullList = await ajax_get(breedsListUrl, "GET");
+    breeds.fullList = await ajax(breedsListUrl, "GET");
     breeds.fullList = breeds.fullList.data;
     for (let item of breeds.fullList/*breedsObj.data*//*breeds.list*/) {
         option = document.createElement("option");
@@ -482,7 +575,7 @@ for (let button of buttons) {
         breedsBreeds.appendChild(option.cloneNode(true));
 
         if (!item.image) {
-            item.image = await ajax_get(`https://api.thecatapi.com/v1/images/search?size=thumb&order=ASC&breed_id=${
+            item.image = await ajax(`https://api.thecatapi.com/v1/images/search?size=thumb&order=ASC&breed_id=${
                     item.id}`, "GET");
             item.image = item.image.data[0];
         }
